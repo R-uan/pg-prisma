053f39a844d94c1ebecacad5aedda386f22565b5
Ruan Lopes <ruan.oliveiralopes@Hotmail.com>
2023-07-10 05:14:13 -0300

    small changes

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-import {
-    getAll,
-    postOne,
-    getOneUsername,
-    deleteByEmail,
-    updateByEmail,
-} from "../sgbd/controllers.js";
+import { getAll, postOne, getOneUsername, deleteByEmail, updateByEmail } from "../sgbd/control.js";
 
 router.route("/contacts").get(getAll).post(postOne);
 router.route("/contacts/:id").get(getOneUsername).delete(deleteByEmail).patch(updateByEmail);
